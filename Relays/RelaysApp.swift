50ee66802b5eea8415e78dfd3010cd4c3eb6be24
Benjamin Stiller <86909585+stillerbenjamin@users.noreply.github.com>
2026-08-29 05:17:29 +0200

    Initial Commit

---
+++ Relays/RelaysApp.swift
@@ -0,0 +1,17 @@
+//
+//  RelaysApp.swift
+//  Relays
+//
+//  Created by Benjamin Stiller on 29.08.26.
+//
+
+import SwiftUI
+
+@main
+struct RelaysApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ RelaysTests/RelaysTests.swift
@@ -0,0 +1,16 @@
+//
+//  RelaysTests.swift
+//  RelaysTests
+//
+//  Created by Benjamin Stiller on 29.08.26.
+//
+
+import Testing
+
+struct RelaysTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
